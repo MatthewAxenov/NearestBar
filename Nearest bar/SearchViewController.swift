b9703215b419ae5d94f8ee096035bac018ea7270
MatthewAxenov <matthewaxenov18@gmail.com>
2022-04-04 16:38:15 +0300

    Добавил экран поиска
    эп

---
+++ Nearest bar/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  Nearest bar
+//
+//  Created by Матвей on 04.04.2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        searchButton.layer.cornerRadius = 15
+    }
+    
+
+    
+}
